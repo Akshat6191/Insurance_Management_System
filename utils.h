5683f0cb5d80fa423f2c0446c4d9ec6c4d38d7bb
Akshat6191 <akshatmankotia@gmail.com>
2026-03-01 23:12:42 +0530

    Edited utils.h

--- utils.h
+++ utils.h
@@ -1,6 +1,25 @@
 #ifndef UTILS_H
 #define UTILS_H 
 
+#include<string>
+
+namespace Utils{
+
+    void clear_Screen();
+    void pause_Screen();
+    void print_Line(char ch = '-' , int l = 70);
+    void print_Header(const std::string& title);
+    
+
+    int get_Int_Input(const std::string& labels , int min_val , int max_value);
+    double get_Double_Input(const std::string labels , double min_val);
+    std::string get_String_Input(const std::string& labels);
+
+    std::string generate_Policy_Number(const std::string& labels , int id);
+    
+}
+
+
     
 
 
