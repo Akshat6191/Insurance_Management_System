d6abb467cea7c5d9f4602e5a70c03d8fa0a4d427
Akshat6191 <akshatmankotia@gmail.com>
2026-03-06 11:03:20 +0530

    edited my name

--- LifeInsurancePolicy.cpp
+++ LifeInsurancePolicy.cpp
@@ -13,11 +13,11 @@ LifeInsurancePolicy::LifeInsurancePolicy(std::string pol_no_,
                                         double       sum_assured_,
                                         int          term_years_,
                                         std::string  nomineeName)
-InsurancePolicy(std::move(pol_no_),
+ : InsurancePolicy(std::move(pol_no_),
                 std::move(holder_name_),
                 holder_age_,
-                sumAssured)
-, term_years_  (term_years_)
+                sum_assured_)
+, termYears_   (term_years_)
 , nomineeName_ (std::move(nomineeName))
 {
     if (term_years_ <=0 || term_years_ >40)
@@ -25,21 +25,23 @@ InsurancePolicy(std::move(pol_no_),
 }                
 double LifeInsurancePolicy::calculatePremium() const {
     const double ageRate    = getholder_age_() * 0.0005;
-    const double termFactor = term_years_      * 0.001;
-    return getsumAssured() * (ageRate + termFactor);}
+    const double termFactor = termYears_       * 0.001;
+    return getsum_assured_() * (ageRate + termFactor);
+}
 
-    std::string LifeInsurancePolicy::displayDetails() const {
-        return "Life Insurance";}
+std::string LifeInsurancePolicy::getpolicy_type_() const {
+    return "Life Insurance";
+}
 
-        void LifeInsurancePolicy::displayDetails() const {
-        utils::printLine();
+void LifeInsurancePolicy::displayDetails() const {
+        utils::print_Line();
         displayCommon();
-        std::cout << "Policy Term    : " <<term_years_ << "years\n";
-        std::cout << "Nominee Name   : " <<nomineeName_ << "years\n";
+        std::cout << "Policy Term    : " << termYears_ << " years\n";
+        std::cout << "Nominee Name   : " << nomineeName_ << "\n";
         std::cout << std::fixed << std::setprecision(2);
-        std::cout << " Annual Premium : Rs. " << calculatePremium()  <<'\n';
-        std::cout << " Monthly Premium : Rs. " << calculatePremium()  <<'\n';
-        utils::printLine();
+        std::cout << " Annual Premium : Rs. " << calculatePremium()  << '\n';
+        std::cout << " Monthly Premium : Rs. " << calculatePremium() / 12  << '\n';
+        utils::print_Line();
     }
 int LifeInsurancePolicy::getTermYears() const noexcept {
     return termYears_;
--- LifeInsurancePolicy.h
+++ LifeInsurancePolicy.h
@@ -5,22 +5,33 @@
 #include <string>
 
 class LifeInsurancePolicy final : public InsurancePolicy{
+
+    private:
+        int           termYears_;
+        std::string   nomineeName_;
+
     public:
     LifeInsurancePolicy(std::string pol_no_,
                         std::string holder_name_,
                         int         holder_age_,
-                        double      sumAssured,
+                        double      sum_assured_,
+                        int         term_years_,
                         std::string nomineeName);
 
+    void settermYears_(int termYears);
+    void setnomineeName_(std::string nomineeName);
+
 double       calculatePremium() const override;
-std::string  getPolicyType()    const override;
+std::string  getpolicy_type_() const override;
 void         displayDetails()   const override;
 
 int                 getTermYears()   const noexcept;
 const std::string&  getNomineeName() const noexcept;
 
-private:
-int           termYears_;
-std::string   nomineeName_;
+bool operator == (const LifeInsurancePolicy& lic)const;
+std::partial_ordering operator <=>(const LifeInsurancePolicy& lic) const;
+std::ostream& print(std::ostream& os) const;
 };
+
+inline std::ostream& operator << (std::ostream& os , const LifeInsurancePolicy& lic) {return lic.print(os);}
 #endif
